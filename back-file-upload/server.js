7787c05cb2110c08e7ce92e3e9901bdcb7242398
Ricky Melida <ricardomelida92.trabajo@gmail.com>
2020-07-08 05:52:25 -0400

    Cambios pormenores

--- back-file-upload/server.js
+++ back-file-upload/server.js
@@ -1,6 +1,7 @@
 const express = require('express');  
 const cors = require('cors');
 const app = express();  
+const fs = require('fs');
 
 const PORT = 3000;
 
@@ -19,9 +20,23 @@ app.use(bodyParser.urlencoded({
 }));
 
 app.post('/api/upload', multipartMiddleware, (req, res, next) => {  
+    let nombre = req.body;
     res.json({
-        'message': 'File uploaded succesfully.'
+        'message': 'File uploaded succesfully.',
+        'data': req.body,
+        'file': req.files.file
     });
+
+    console.log(req.body.sale);
+    fs.rename(req.files.upload.path, 'uploads/nuevo.mp3',(err)=>{
+        if(err) throw err;
+        console.log('Se modifico el nombre');
+    })
+    //renombrar(req.files.file.path);
 });
+
+ renombrar = (nombre)=> {
+    console.log(`Se va a renombrar ${nombre}`);
+}
 
 app.listen(PORT, () => console.log(`Example app listening on PORT ${PORT}!`))
